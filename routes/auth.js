913910ebd73f7dfb50cfc1931e78816e3737575d
Gustavo <gar.santeliz@gmail.com>
2022-07-30 20:21:01 -0400

    routes created and middlewares passed to validate data

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,28 @@
-const { Router } = require('express')
-const router = Router()
+const { Router } = require("express");
+const router = Router();
+const { check } = require("express-validator");
 
+const { createUser, loginUser, renewToken } = require("../controllers/auth");
+const validateFields = require("../middlewares/validate-field");
 
-router.get('/' , ( reequire, response ) => {
-    response.json({
-        ok: true
-    })
-})
+router.post(
+        "/new", 
+    [ //middelwares para validar 
+        check('name', 'el nombre es obligatorio').not().isEmpty(),
+        check('email', 'el email es obligatorio').isEmail(),
+        check('password', 'el password es debe ser superior a 6 caracteres').isLength({ min: 6}),
+        validateFields
+    ], 
+        createUser);
 
-module.exports = router
+router.post("/", 
+[ //middelwares para validar 
+        check('email', 'el email es obligatorio').isEmail(),
+        check('password', 'el password es debe ser superior a 6 caracteres').isLength({ min: 6}),
+        validateFields
+    ], 
+ loginUser);
+
+router.get("/renew", [], renewToken);
+
+module.exports = router;
