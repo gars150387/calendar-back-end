2fd77634c7354488f7888da3b71104daf5714a47
Gustavo <gar.santeliz@gmail.com>
2022-08-01 22:40:24 -0400

    events functions as creating, editing and deleting set up

--- controllers/Events.js
+++ controllers/Events.js
@@ -5,6 +5,7 @@ const Event = require('../models/Event')
 const getEvents = async ( request, response = response ) => {
 
     const events = await Event.find()
+                                .populate('user', 'name') //to bring more info of the event
 
     return response.json({
         ok: true,
@@ -45,22 +46,100 @@ const createEvent = async ( request, response = response ) => {
 }
 
 
-const editEvent = ( request, response = response ) => {
+const editEvent = async ( request, response = response ) => {
 
-    return response.json({
-        ok: true,
-        msg: 'Edit event'
-    })
+    const eventId = request.params.id
+    const uid = request.uid
+
+    console.log( eventId )
+    console.log( uid )
+
+    try {
+
+        const event = await Event.findById( eventId )
+        console.log( event )
+        
+        if ( !event ) {
+            return response.status(404).json({
+                ok: false,
+                msg: 'Event/id do not match'
+            })
+        }
+
+        if ( event.user.toString() !== uid ) {
+            return response.status(401).json({
+                ok: false,
+                msg: 'No auhorize to edit event'
+            })
+        }
+
+
+        const newEvent = {
+            ...request.body,
+            user: uid
+        }
+
+        const eventUpdated = await Event.findByIdAndUpdate( eventId, newEvent, { new: true } )
+
+
+        return response.json({
+            ok: true,
+            event: eventUpdated
+        })
+
+
+
+    } catch (error) {
+        console.log(error)
+        return response.status(500).json({
+            ok: false,
+            msg: 'Contact Administrator'
+        })
+        
+    }
 }
 
 
-const deleteEvent = ( request, response = response ) => {
+const deleteEvent = async ( request, response = response ) => {
 
-    return response.json({
-        ok: true,
-        msg: 'Delete event'
-    })
+    const eventId = request.params.id
+    const uid = request.uid
+
+    console.log( eventId )
+    console.log( uid )
+
+    try {
+
+        const event = await Event.findById( eventId )
+        console.log( event )
+        
+        if ( !event ) {
+            return response.status(404).json({
+                ok: false,
+                msg: 'Event/id do not match'
+            })
+        }
+
+        if ( event.user.toString() !== uid ) {
+            return response.status(401).json({
+                ok: false,
+                msg: 'No auhorize to edit event'
+            })
+        }
+
+        const eventDeleted = await Event.findByIdAndDelete( eventId )
+
+        return response.json({
+            ok: true,
+            eventDeleted
+        })
+
+    } catch(error){
+        console.log(error)
+        msg: 'Please contact administrator'
+    }
 }
+
 
 
 module.exports = {
