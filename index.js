3a64204f45e60336a9331d419d572f64959d9161
Gustavo <gar.santeliz@gmail.com>
2022-08-14 22:14:23 -0400

    routes events changed to lower letters

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use( express.json())
 //rutas
 //TODO: auth // crear, login, renew
 app.use('/api/auth', ( require('./routes/auth')))
-app.use('/api/Events', ( require('./routes/Events')))
+app.use('/api/events', ( require('./routes/events')))
 
 //TODO:  CRUD Events 
 
