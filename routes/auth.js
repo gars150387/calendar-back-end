cc4a1dd1453600d50caa6e87cc36f6988cc4b769
grodriguezcontextglobal <grodriguez@contextglobal.com>
2023-11-17 22:42:24 -0500

    adding repo to heroku

--- routes/auth.js
+++ routes/auth.js
@@ -7,12 +7,7 @@ const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validar-campos');
 const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
 const { validarJWT } = require('../middlewares/validar-jwt');
-
-
 const router = Router();
-
-
-
 router.post(
     '/new', 
     [ // middlewares
